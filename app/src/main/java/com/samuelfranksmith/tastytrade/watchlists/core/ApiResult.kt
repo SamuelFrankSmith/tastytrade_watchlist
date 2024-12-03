4007c9bf77b51a43c93ef6299dca29ebbd52d82d
SamFrankSmith <samfranksmith@gmail.com>
2024-12-03 11:51:32 -0600

    Changed ApiResult to have a single specified generic. This must have been a typo from last week, but I never recognized it and was awfully confused this morning by what my original intention was. I don't think I had any intention other than I mixed up T and R usage.

--- app/src/main/java/com/samuelfranksmith/tastytrade/watchlists/core/ApiResult.kt
+++ app/src/main/java/com/samuelfranksmith/tastytrade/watchlists/core/ApiResult.kt
@@ -1,7 +1,7 @@
 package com.samuelfranksmith.tastytrade.watchlists.core
 
 sealed class ApiResult<out T> {
-    data class Success<out R>(val value: R): ApiResult<R>()
+    data class Success<out T>(val value: T): ApiResult<T>()
     data class Error(
         val code: String? = null,
         val message: String? = null,
